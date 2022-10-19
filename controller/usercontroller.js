bb76a273362a8f8fc5c3462f91b8f1383f8100dd
mangalam2 <mangalam@thoughtwin.com>
2022-10-19 18:03:44 +0530

    delete all applyed

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -1,4 +1,5 @@
 
+const usermodel = require("../model/usermodel");
 const userModel = require("../model/usermodel");
 
 exports.add_user = (request,res)=>{
@@ -42,26 +43,44 @@ exports.usersDetail = (request,response)=>{
 exports.update = (request,res)=>{
     userModel.updateOne({_id:request.params.id},
         {
-           $set : {
-            Product_title:request.body.Product_title,
-            Product_Image:request.body.Product_Image,
-            Product_discription:request.body.Product_discription,
-            Product_rating:request.body.Product_rating,
-            Freshness:request.body.Freshness,
-            Farm:request.body.Farm,
-            Delivery:request.body.Delivery,
-            Stock:request.body.Stock,
-            Product_price:request.body.Product_price
-            }
+           $set : request.body
         }).then(result=>{
             if(result.acknowledged==1){
-                return res.status(201).json({message:"data have been updated"});
+                return res.status(201).json({message:"data has been updated"});
             }
             else{
-                return res.status(500).json({message:"data have been not updated "})
+                return res.status(500).json({message:"data has been not updated "})
             }
         }).catch(err=>{
             return res.status(500).json(err);
         })
         
+}
+
+exports.delete = (req,res)=>{
+    userModel.deleteOne({_id:req.params.id}).then(result=>{
+        if(result.acknowledged==true){
+            return res.status(201).json({message:"data has been deleted"})
+        }
+        else{
+            return res.status(500).json({message:"not deleted"})
+        }
+    }).catch(err=>{
+        return res.status(500).json({err})
+    })
+}
+
+exports.deleteall= (req,res)=>{
+    userModel.deleteMany(res).then(result=>{
+        
+        if(result.send()==true){
+            return res.status(201).json({message:"data has been deleted"})
+        }
+        else{
+            return res.status(500).json({message:"not deleted"})
+        }
+    }).catch(err=>{
+        return res.status(500).json({err})
+    })
+    
 }
--- router/router.js
+++ router/router.js
@@ -5,5 +5,7 @@ router.post("/user-data",usercontroller.add_user);
 router.get("/userdata",usercontroller.users);
 router.get("/userdetails/:id",usercontroller.usersDetail);
 router.put("/update/:id",usercontroller.update )
+router.delete("/delete/:id",usercontroller.delete );
+router.delete("/delete",usercontroller.deleteall );
 
 module.exports = router;
